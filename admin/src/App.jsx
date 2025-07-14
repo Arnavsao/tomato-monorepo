a6f89dea9c4e5a5fe3ec7fdd43a29153c4f199ec
Arnav Sao <140349606+Arnavsao@users.noreply.github.com>
2025-07-14 10:03:16 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Add from './pages/Add/Add.jsx';
-import List from './pages/List/Lisgitt.jsx';
+import List from './pages/List/List.jsx';
 import Orders from './pages/Orders/Orders.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
